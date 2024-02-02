6923d466cd9c73372b5dac3e8255bc04bef1a1fb
Igorello <ioproglab@gmail.com>
2024-02-02 13:39:59 +0300

    added data init

---
+++ app/src/main/java/com/obsessed/e_journal/Data/Data.java
@@ -0,0 +1,92 @@
+package com.obsessed.e_journal.Data;
+
+import com.obsessed.e_journal.School.Employee;
+import com.obsessed.e_journal.School.Learner;
+import com.obsessed.e_journal.School.Parent;
+import com.obsessed.e_journal.School.Section;
+import com.obsessed.e_journal.School.Teacher;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Data {
+    private static volatile Data uniqueData;
+    private final ArrayList<Parent> parentsList;
+    private final ArrayList<Learner> learnersList;
+    private final ArrayList<Teacher> teachersList;
+    private final ArrayList<Employee> empoloyeesList;
+    private final ArrayList<Section> sectionsList;
+
+
+    public static Data getInstance() {
+        if (uniqueData == null) {
+            synchronized (Data.class) {
+                if (uniqueData == null) {
+                    uniqueData = new Data();
+                }
+            }
+        }
+        return uniqueData;
+    }
+
+    private Data(){
+        //Parents
+        parentsList = new ArrayList<>();
+        parentsList.add(new Parent("Иванов Иван Иванович", 9123456789L));
+        parentsList.add(new Parent("Петрова Екатерина Алексеевна", 9124567890L));
+        parentsList.add(new Parent("Смирнов Сергей Владимирович", 9345678901L));
+        parentsList.add(new Parent("Кузнецова Анастасия Петровна", 9346789012L));
+        parentsList.add(new Parent("Федоров Дмитрий Павлович", 9567890123L));
+        parentsList.add(new Parent("Морозова Людмила Александровна", 9568901234L));
+        parentsList.add(new Parent("Егоров Андрей Николаевич", 9789012345L));
+        parentsList.add(new Parent("Волкова Ольга Сергеевна", 9780123456L));
+        parentsList.add(new Parent("Никитин Максим Алексеевич", 9901234567L));
+        parentsList.add(new Parent("Соловьева Татьяна Владимировна", 9902345678L));
+
+
+        //Learners
+        learnersList = new ArrayList<>();
+        learnersList.add(new Learner("Иванов Александр Иванович", 9125678901L, 70001, new ArrayList<>(Arrays.asList(parentsList.get(0), parentsList.get(1)))));
+        learnersList.add(new Learner("Иванов Артем Иванович", 9126789012L, 70002, new ArrayList<>(Arrays.asList(parentsList.get(0), parentsList.get(1)))));
+        learnersList.add(new Learner("Смирнов Денис Сергеевич", 9346789012L, 70003, new ArrayList<>(Arrays.asList(parentsList.get(2), parentsList.get(3)))));
+        learnersList.add(new Learner("Федоров Михаил Дмитриевич", 9566789012L, 70004, new ArrayList<>(Arrays.asList(parentsList.get(4), parentsList.get(5)))));
+        learnersList.add(new Learner("Егорова Екатерина Андреевна", 9786789012L, 70005, new ArrayList<>(Arrays.asList(parentsList.get(6), parentsList.get(7)))));
+        learnersList.add(new Learner("Никитина Виктория Максимовна", 9906789012L, 70006, new ArrayList<>(Arrays.asList(parentsList.get(8), parentsList.get(9)))));
+
+        //Teachers
+        teachersList = new ArrayList<>();
+        teachersList.add(new Teacher("Семенова Анна Васильевна", 9890123456L, 10001, "Учитель математики", "Высшая категория"));
+        teachersList.add(new Teacher("Петров Петр Петрович", 9891234567L, 10002, "Учитель физики", "Высшая категория"));
+        teachersList.add(new Teacher("Иванова Елена Сергеевна ", 9892345678L, 10003, "Учитель литературы", "Молодой специалист"));
+
+        //Empoloyees
+        empoloyeesList = new ArrayList<>();
+        empoloyeesList.add(new Employee("Николаева Марина Александровна", 9894567890L, 20001, "Администратор"));
+        empoloyeesList.add(new Employee("Сидоров Алексей Владимирович", 9896543210L, 20003, "Уборщик"));
+        empoloyeesList.add(new Employee("Кузнецова Татьяна Петровна", 9891234567L, 20004, "Кухарка"));
+
+        //Sections
+        sectionsList = new ArrayList<>();
+        sectionsList.add(new Section("Маткульт", new ArrayList<>(Arrays.asList(learnersList.get(0), learnersList.get(1), learnersList.get(2), learnersList.get(3))), teachersList.get(0)));
+    }
+
+    public ArrayList<Parent> getParentsList() {
+        return parentsList;
+    }
+
+    public ArrayList<Learner> getLearnersList() {
+        return learnersList;
+    }
+
+    public ArrayList<Teacher> getTeachersList() {
+        return teachersList;
+    }
+
+    public ArrayList<Employee> getEmpoloyeesList() {
+        return empoloyeesList;
+    }
+
+    public ArrayList<Section> getSectionsList() {
+        return sectionsList;
+    }
+}
